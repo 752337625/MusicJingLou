99bbf668259ffb91fa3f1e56913a219d31627106
whw <whw520byl@163.com>
2023-04-06 22:17:55 +0800

    ci:  自动化构建

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,4 +6,9 @@ pnpm i
 
 pnpm run electron:build
 
+
+cp dist_electron/vite-electron-plugin-setup.exe /usr/local/NeteaseCloudMusicApi/public
+
+cp dist_electron/latest.yml /usr/local/NeteaseCloudMusicApi/public
+
 echo "✅ Publish completed"
