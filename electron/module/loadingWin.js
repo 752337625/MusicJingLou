dbab8debc60f5934872bc41f16ce024e222dfcf2
whw <whw520byl@163.com>
2023-04-09 23:14:59 +0800

    feat: 重新构建

--- electron/module/loadingWin.js
+++ electron/module/loadingWin.js
@@ -1,19 +1,23 @@
 const { BrowserWindow } = require('electron');
+const url = require('url');
 const { LOAD_URL_MAIN, isPro, LOADING_URL_MAIN_HASH } = require('../config');
-function createLoading(cd) {
+function createLoading(cb) {
+  const icon = isPro ? `${global.__images}/icon.png` : 'public/images/icon.png';
   global.loading = new BrowserWindow({
     show: false,
     frame: false, // 无边框（窗口、工具栏等），只包含网页内容
     width: 240,
-    height: 500,
+    height: 450,
     movable: true,
+    icon: icon,
     minimizable: false,
     maximizable: false,
     resizable: false,
     alwaysOnTop: true,
-    skipTaskbar: true,
+    skipTaskbar: false,
     transparent: true, // 窗口是否支持透明，如果想做高级效果最好为true
   });
+
   if (isPro) {
     global.loading.loadFile(LOAD_URL_MAIN, {
       hash: url.format(LOADING_URL_MAIN_HASH),
@@ -22,7 +26,7 @@ function createLoading(cd) {
     global.loading.loadURL(LOADING_URL_MAIN_HASH);
   }
   global.loading.show();
-  global.loading.once('show', cd);
+  global.loading.once('show', cb);
 }
 
 module.exports = { createLoading };
