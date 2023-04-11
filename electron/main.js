513acf5a2eaf613b793d171d4a3c7f97df34751f
Mr. Wang Hong Wei <15536300457@163.com>
2023-04-11 17:36:49 +0800

    feat: 自动更新

--- electron/main.js
+++ electron/main.js
@@ -108,32 +108,6 @@ function createWindow() {
   //  开机自启
   checkIsAutoLaunch();
 }
-// function createLoading(cb) {
-//   global.loading = new BrowserWindow({
-//     show: false,
-//     frame: false, // 无边框（窗口、工具栏等），只包含网页内容
-//     width: 240,
-//     height: 500,
-//     movable: true,
-//     minimizable: false,
-//     maximizable: false,
-//     resizable: false,
-//     alwaysOnTop: true,
-//     skipTaskbar: true,
-//     transparent: true, // 窗口是否支持透明，如果想做高级效果最好为true
-//   });
-//   if (isPro) {
-//     global.loading.loadFile(LOAD_URL_MAIN, {
-//       hash: url.format(LOADING_URL_MAIN_HASH),
-//     });
-//   } else {
-//     global.loading.loadURL(LOADING_URL_MAIN_HASH);
-//   }
-//   global.loading.once('ready-to-show', () => {
-//     global.loading.show();
-//   });
-//   global.loading.once('show', cb);
-// }
 
 app.whenReady().then(() => {
   if (isPro) return createLoading(createWindow());
@@ -146,7 +120,7 @@ app.on('activate', () => {
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') return app.quit();
 });
-// app.setLoginItemSettings({ openAtLogin: true }); // 开机自启
+
 const gotTheLock = app.requestSingleInstanceLock();
 if (!gotTheLock) {
   app.quit();
--- scripts/installer.nsh
+++ scripts/installer.nsh
@@ -1,7 +1,16 @@
 #installer.nsh
 
 !macro customInit
-    # 修改默认安装路径
-    StrCpy $INSTDIR "$LocalAppData\Programs\${APP_FILENAME}"
+  # 修改默认安装路径
+  StrCpy $INSTDIR "$LocalAppData\Programs\${APP_FILENAME}"
 !macroend
 
+
+!macro customInstall
+  WriteRegStr HKCR "epis_auto_file\shell\open\command" "" '"$INSTDIR\${PRODUCT_NAME}.exe" "%1"'
+  WriteRegStr HKCR "epis_auto_file\DefaultIcon" "" '$INSTDIR\${PRODUCT_NAME}.exe,0'
+!macroend
+
+!macro customUnInstall
+  DeleteRegKey HKCR "epis_auto_file"
+!macroend
